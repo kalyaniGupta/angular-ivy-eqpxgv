465aae8aff41dbf22d194417ef003141f9c11973
kalyaniGupta <kalyanigupta13@gmail.com>
2022-07-11 17:17:49 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import { Router } from '@angular/router';
+import { BnNgIdleService } from 'bn-ng-idle';
 
 import { AuthService } from './_services/auth.service';
 
@@ -11,7 +12,22 @@ import { AuthService } from './_services/auth.service';
 export class AppComponent {
   title = 'Routing Module - Route Guards Demo';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private bnIdle: BnNgIdleService
+  ) {}
+
+  // initiate it in your component OnInit
+  ngOnInit(): void {
+    this.bnIdle.startWatching(60).subscribe((isTimedOut: boolean) => {
+      if (isTimedOut) {
+        console.log('session expired');
+        this.logout();
+        this.router.navigate(['logout']);
+      }
+    });
+  }
 
   logout() {
     this.authService.logoutUser();
